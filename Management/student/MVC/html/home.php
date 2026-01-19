35efae1983167754ac159c3970f7b3c200606fb3
tafsin17 <tahomina.tafsin@gmail.com>
2026-01-19 18:51:19 +0600

    home

---
+++ Management/student/MVC/html/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AIUB Student Notes</title>
+    <link rel="stylesheet" href="../css/home.css">
+</head>
+<body>
+    <header class="hero">
+        
+    </header>
+</body>
+</html>
