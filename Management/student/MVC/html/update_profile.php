35e7ddaa60494cf86c7d33763d1f3a37a85d7f67
tafsin17 <tahomina.tafsin@gmail.com>
2026-01-19 19:18:18 +0600

    update profile

--- Management/student/MVC/html/update_profile.php
+++ Management/student/MVC/html/update_profile.php
@@ -6,7 +6,11 @@
 </head>
 <body>
     <form method="POST" action="../php/update_profile.php">
-        
+        <input type="text" name="full_name" placeholder="New Name">
+        <input type="email" name="email" placeholder="New Email">
+        <input type="password" name="current_password" placeholder="Current Password" required>
+        <input type="password" name="new_password" placeholder="New Password">
+        <button type="submit" name="update">Update</button>
     </form>
 </body>
 </html>
