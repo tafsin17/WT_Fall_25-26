a800479d86731b2681f8943ef70b06223dfb76b5
tafsin17 <tahomina.tafsin@gmail.com>
2026-01-19 23:57:59 +0600

    db

--- Management/student/MVC/db/db.php
+++ Management/student/MVC/db/db.php
@@ -7,5 +7,6 @@ $dbname = "aiub_student_management";
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
-    
+    die("Connection failed: " . $conn->connect_error);
+
 ?>
