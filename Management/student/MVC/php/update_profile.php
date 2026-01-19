348a957b7c896b223591c740aaf83537ff085a73
tafsin17 <tahomina.tafsin@gmail.com>
2026-01-19 23:29:37 +0600

    updateprofile

--- Management/student/MVC/php/update_profile.php
+++ Management/student/MVC/php/update_profile.php
@@ -2,5 +2,29 @@
 session_start();
 include "../db/db.php";
 
+if (isset($_POST['update'])) {
+    $id = $_SESSION['user_id'];
+    $name = mysqli_real_escape_string($conn, $_POST['full_name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $curr_pass = $_POST['current_password'];
+    $new_pass = $_POST['new_password'];
 
+    $sql = "SELECT * FROM students WHERE id = '$id'";
+    $res = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_assoc($res);
+
+    if ($curr_pass !== $user['password']) {
+        header("Location: ../html/update_profile.php?error=1");
+        exit();
+    }
+
+    $update_pass = !empty($new_pass) ? $new_pass : $curr_pass;
+    $update_sql = "UPDATE students SET full_name='$name', email='$email', password='$update_pass' WHERE id='$id'";
+
+    if (mysqli_query($conn, $update_sql)) {
+        $_SESSION['full_name'] = $name;
+        header("Location: ../html/update_profile.php?success=1");
+    }
+    exit();
+}
 ?>
