50034e1f9b39b2cdada569c4d0634761538b8736
tafsin17 <tahomina.tafsin@gmail.com>
2026-01-19 19:17:36 +0600

    update profile

---
+++ Management/student/MVC/html/update_profile.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Update Profile</title>
+    <link rel="stylesheet" href="../css/update_profile.css">
+</head>
+<body>
+    <form method="POST" action="../php/update_profile.php">
+        
+    </form>
+</body>
+</html>
