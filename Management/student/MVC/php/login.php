a281336c52750d13840e1ab175ce968dcaa18da3
tafsin17 <tahomina.tafsin@gmail.com>
2026-01-19 23:23:53 +0600

    login

--- Management/student/MVC/php/login.php
+++ Management/student/MVC/php/login.php
@@ -9,5 +9,17 @@ if (isset($_POST['login'])) {
     $sql = "SELECT * FROM students WHERE email='$email' AND password='$password'";
     $result = mysqli_query($conn, $sql);
 
+    if (mysqli_num_rows($result) == 1) {
+        $user = mysqli_fetch_assoc($result);
+        $_SESSION['user_type'] = 'student';
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['full_name'] = $user['full_name'];
+
+        header("Location: ../html/dashboard.php");
+        exit();
+    } else {
+        header("Location: ../html/login.php?error=1");
+        exit();
+    }
 }
 ?>
