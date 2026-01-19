fdd8a90afd4300db6ca642708a2c5ff1dea13403
tafsin17 <tahomina.tafsin@gmail.com>
2026-01-19 18:29:56 +0600

    dashboard

---
+++ Management/student/MVC/html/dashboard.php
@@ -0,0 +1,15 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../css/dashboard.css">
+</head>
+<body>
+    <div class="navbar">
+        <h2>AIUB Notes</h2>
+        <a href="../php/logout.php">Logout</a>
+    </div>
+    
+</body>
+</html>
